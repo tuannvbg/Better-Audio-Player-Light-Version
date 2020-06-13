90fbfd69664032f0236eec2b15387e6ef8f8aa52
tuannvbg <nguyentuan2005@gmail.com>
2020-06-13 09:09:24 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,276 @@
+/* Music 
+======================================*/
+var playlist = [
+	{
+		"song"    : "Mỹ Nhân",
+		"album"   : "Nhạc Trẻ Tuyển Chọn Năm 2020",
+		"artist"  : "Đinh Đại Vũ",
+		"artwork" : "https://archive.org/download/dinh.-dai.-vu.-avatar.-zing-mp-3/Dinh.Dai.Vu.Avatar.ZingMP3.jpg",
+		"mp3"     : "https://archive.org/download/dinh.-dai.-vu-my.-nhan/Dinh.Dai.Vu-My.Nhan.mp3"
+	},
+	{
+		"song"    : "Từng Yêu",
+		"album"   : "Nhạc Trẻ Tuyển Chọn Năm 2020",
+		"artist"  : "Phan Duy Anh",
+		"artwork" : "https://archive.org/download/phan.-duy.-anh.-avatar/Phan.Duy.Anh.Avatar.jpg",
+		"mp3"     : "https://archive.org/download/phan.-duy.-anh-tung.-yeu/Phan.Duy.Anh-Tung.Yeu.mp3"
+	},
+	{
+		"song"    : "Anh Thanh Niên",
+		"album"   : "Nhạc Trẻ Tuyển Chọn Năm 2020",
+		"artist"  : "Huy R",
+		"artwork" : "https://archive.org/download/huy-r.-avatar.-zing-mp-3/HuyR.Avatar.ZingMP3.jpg	",
+		"mp3"     : "https://archive.org/download/huy-r-anh.-thanh.-nien/HuyR-Anh.Thanh.Nien.mp3"
+	},
+	{
+		"song"    : "Tình Sầu Thiên Thu Muôn Lối",
+		"album"   : "The Best Of Triệu Hoàng",
+		"artist"  : "Doãn Hiếu",
+		"artwork" : "https://archive.org/download/doan.-hieu.-avatar-2/Doan.Hieu.Avatar2.jpg",
+		"mp3"     : "https://archive.org/download/doan.-hieu-tinh.-sau.-thien.-thu.-muon.-loi/Doan.Hieu-Tinh.Sau.Thien.Thu.Muon.Loi.mp3"
+	}
+];
+
+/* General Load / Variables
+======================================*/
+var rot = 0;
+var duration;
+var playPercent;
+var rotate_timer;
+var armrot = -45;
+var bufferPercent;
+var currentSong = 0;
+var arm_rotate_timer;
+var arm = document.getElementById("arm");
+var next = document.getElementById("next");
+var song = document.getElementById("song");
+var timer = document.getElementById("timer");
+var music = document.getElementById("music");
+var album = document.getElementById("album");
+var artist = document.getElementById("artist");
+var volume = document.getElementById("volume");
+var playButton = document.getElementById("play");
+var timeline = document.getElementById("slider");
+var playhead = document.getElementById("elapsed");
+var previous = document.getElementById("previous");
+var pauseButton = document.getElementById("pause");
+var bufferhead = document.getElementById("buffered");
+var artwork = document.getElementsByClassName("artwork")[0];
+var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
+var visablevolume = document.getElementsByClassName("volume")[0];
+
+music.addEventListener("ended", _next, false);
+music.addEventListener("timeupdate", timeUpdate, false);
+music.addEventListener("progress", 	bufferUpdate, false);
+load();
+
+/* Functions
+======================================*/
+function load(){
+	pauseButton.style.visibility = "hidden";
+	song.innerHTML = playlist[currentSong]['song'];
+	song.title = playlist[currentSong]['song'];
+	album.innerHTML = playlist[currentSong]['album'];
+	album.title = playlist[currentSong]['album'];
+	artist.innerHTML = playlist[currentSong]['artist'];
+	artist.title = playlist[currentSong]['artist'];
+	artwork.setAttribute("style", "background:url(./images/3idGgyU.png), url('"+playlist[currentSong]['artwork']+"') center no-repeat;");
+	music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+	music.load();
+}
+function reset(){ 
+	rotate_reset = setInterval(function(){ 
+		Rotate();
+		if(rot == 0){
+			clearTimeout(rotate_reset);
+		}
+	}, 1);
+	fireEvent(pauseButton, 'click');
+	armrot = -45;
+	playhead.style.width = "0px";
+	bufferhead.style.width = "0px";
+	timer.innerHTML = "0:00";
+	music.innerHTML = "";
+	currentSong = 0; // set to first song, to stay on last song: currentSong = playlist.length - 1;
+	song.innerHTML = playlist[currentSong]['song'];
+	song.title = playlist[currentSong]['song'];
+	album.innerHTML = playlist[currentSong]['album'];
+	album.title = playlist[currentSong]['album'];
+	artist.innerHTML = playlist[currentSong]['artist'];
+	artist.title = playlist[currentSong]['artist'];
+	artwork.setAttribute("style", "background:url(images/3idGgyU.png), url('"+playlist[currentSong]['artwork']+"') center no-repeat;");
+	music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+	music.load();
+}
+function formatSecondsAsTime(secs, format) {
+  var hr  = Math.floor(secs / 3600);
+  var min = Math.floor((secs - (hr * 3600))/60);
+  var sec = Math.floor(secs - (hr * 3600) -  (min * 60));
+  if (sec < 10){ 
+    sec  = "0" + sec;
+  }
+  return min + ':' + sec;
+}
+function timeUpdate() {
+	bufferUpdate();
+	playPercent = timelineWidth * (music.currentTime / duration);
+	playhead.style.width = playPercent + "px";
+	timer.innerHTML = formatSecondsAsTime(music.currentTime.toString());
+}
+function bufferUpdate() {
+	bufferPercent = timelineWidth * (music.buffered.end(0) / duration);
+	bufferhead.style.width = bufferPercent + "px";
+}
+function Rotate(){
+	if(rot == 361){
+		artwork.style.transform = 'rotate(0deg)';
+		rot = 0;
+	} else {
+		artwork.style.transform = 'rotate('+rot+'deg)';
+		rot++;
+	}
+}
+function RotateArm(){
+	if(armrot > -12){
+		arm.style.transform = 'rotate(-38deg)';
+		armrot = -45;
+	} else {
+		arm.style.transform = 'rotate('+armrot+'deg)';
+		armrot = armrot + (26 / duration);
+	}
+}
+function fireEvent(el, etype){
+	if (el.fireEvent) {
+		el.fireEvent('on' + etype);
+	} else {
+		var evObj = document.createEvent('Events');
+		evObj.initEvent(etype, true, false);
+		el.dispatchEvent(evObj);
+	}
+}
+function _next(){
+	if(currentSong == playlist.length - 1){
+		reset();
+	} else {
+		fireEvent(next, 'click');
+	}
+}
+playButton.onclick = function() {
+	music.play();
+}
+pauseButton.onclick = function() {
+	music.pause();
+}
+music.addEventListener("play", function () {
+	playButton.style.visibility = "hidden";
+	pause.style.visibility = "visible";
+	rotate_timer = setInterval(function(){ 
+		if(!music.paused && !music.ended && 0 < music.currentTime){
+			Rotate();
+		}
+	}, 10);	
+	if(armrot != -45){
+		arm.setAttribute("style", "transition: transform 800ms;");
+		arm.style.transform = 'rotate('+armrot+'deg)';
+	}
+	arm_rotate_timer = setInterval(function(){ 
+		if(!music.paused && !music.ended && 0 < music.currentTime){
+			if(armrot == -45){
+				arm.setAttribute("style", "transition: transform 800ms;");
+				arm.style.transform = 'rotate(-38deg)';
+				armrot = -38;
+			}
+			if(arm.style.transition != ""){
+				setTimeout(function(){
+					arm.style.transition = "";
+				}, 1000);
+			}
+			RotateArm();
+		}
+	}, 1000);
+}, false);
+music.addEventListener("pause", function () {
+	arm.setAttribute("style", "transition: transform 800ms;");
+	arm.style.transform = 'rotate(-45deg)';
+	playButton.style.visibility = "visible";
+	pause.style.visibility = "hidden";
+	clearTimeout(rotate_timer);
+	clearTimeout(arm_rotate_timer);
+}, false);
+next.onclick = function(){
+	arm.setAttribute("style", "transition: transform 800ms;");
+	arm.style.transform = 'rotate(-45deg)';
+	clearTimeout(rotate_timer);
+	clearTimeout(arm_rotate_timer);
+	playhead.style.width = "0px";
+	bufferhead.style.width = "0px";
+	timer.innerHTML = "0:00";
+	music.innerHTML = "";
+	arm.style.transform = 'rotate(-45deg)';
+	armrot = -45;
+	if((currentSong + 1) == playlist.length){
+		currentSong = 0;
+		music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+	} else {
+		currentSong++;
+		music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+	}
+	song.innerHTML = playlist[currentSong]['song'];
+	song.title = playlist[currentSong]['song'];
+	album.innerHTML = playlist[currentSong]['album'];
+	album.title = playlist[currentSong]['album'];
+	artist.innerHTML = playlist[currentSong]['artist'];
+	artist.title = playlist[currentSong]['artist'];
+	artwork.setAttribute("style", "transform: rotate("+rot+"deg); background:url(images/3idGgyU.png), url('"+playlist[currentSong]['artwork']+"') center no-repeat;");
+	music.load();
+	duration = music.duration;
+	music.play();
+}
+previous.onclick = function(){
+	arm.setAttribute("style", "transition: transform 800ms;");
+	arm.style.transform = 'rotate(-45deg)';
+	clearTimeout(rotate_timer);
+	clearTimeout(arm_rotate_timer);
+	playhead.style.width = "0px";
+	bufferhead.style.width = "0px";
+	timer.innerHTML = "0:00";
+	music.innerHTML = "";
+	arm.style.transform = 'rotate(-45deg)';
+	armrot = -45;
+	if((currentSong - 1) == -1){
+		currentSong = playlist.length - 1;
+		music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+	} else {
+		currentSong--;
+		music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+	}
+	song.innerHTML = playlist[currentSong]['song'];
+	song.title = playlist[currentSong]['song'];
+	album.innerHTML = playlist[currentSong]['album'];
+	album.title = playlist[currentSong]['album'];
+	artist.innerHTML = playlist[currentSong]['artist'];
+	artist.title = playlist[currentSong]['artist'];
+	artwork.setAttribute("style", "transform: rotate("+rot+"deg); background:url(images/3idGgyU.png), url('"+playlist[currentSong]['artwork']+"') center no-repeat;");
+	music.load();
+	duration = music.duration;
+	music.play();
+}
+volume.oninput = function(){
+	music.volume = volume.value;
+	visablevolume.style.width = (80 - 11) * volume.value + "px";
+}
+music.addEventListener("canplay", function () {
+	duration = music.duration;
+}, false);
+$('.marquee').marquee({
+    //speed in milliseconds of the marquee
+    duration: 5000,
+    //gap in pixels between the tickers
+    gap: 50,
+    //time in milliseconds before the marquee will start animating
+    delayBeforeStart: 0,
+    //'left' or 'right'
+    direction: 'left',
+    //true or false - should the marquee be duplicated to show an effect of continues flow
+    duplicated: true
+});
